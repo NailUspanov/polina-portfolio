f007275be4b7396801049d938c3efbe1df620fc1
UspanovNN <nail.uspanov@vitech.team>
2025-07-12 23:29:58 +0400

    Configure custom domain zotovapln.ru

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/polina-portfolio/',
+  base: '/', // Изменено с '/polina-portfolio/' на '/' для кастомного домена
 })
